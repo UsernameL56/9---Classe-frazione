f907085c883f8054f0deb38b7793f13e2ee8f0d2
Corsi.20188 <Corsi.20188@E2-9PCWS2>
2023-10-31 13:30:58 +0100

    Costruttore, Costruttore di copia e metodi accessor

--- classeFrazioni/Class1.cs
+++ classeFrazioni/Class1.cs
@@ -10,5 +10,28 @@ namespace classeFrazioni
     {
     }
 
+    public class Frazione
+    {
+        // attributi
+        private int _numeratore;
+        private int _denominatore;
 
+        // costruttore
+        public Frazione(int _numeratore, int _denominatore) 
+        {
+            Numeratore = _numeratore;
+            Denominatore = _denominatore;
+        }
+        //costruttore di copia
+        public Frazione(Frazione frazionePrecedente)
+        {
+            Numeratore = frazionePrecedente.Numeratore;
+            Denominatore = frazionePrecedente.Denominatore;
+        }
+
+        // metodi accessor
+        public int Numeratore { get { return _numeratore; } set { _numeratore = value; } }
+        public int Denominatore { get { return _denominatore; } set { _denominatore = value; } }
+
+    }
 }
